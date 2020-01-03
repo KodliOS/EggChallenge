ba7a0373a3ad9a90e1aa6b09bae3f986dfccb55c
Hüsamettin Eyibil <heyibil76@gmail.com>
2020-01-04 02:26:18 +0300

    Complete challenge and add completion alert.

--- husamettin/EggChallenge/EggChallenge/Controller/ClockViewController.swift
+++ husamettin/EggChallenge/EggChallenge/Controller/ClockViewController.swift
@@ -9,21 +9,92 @@
 import UIKit
 import Clocket
 
+enum clockState {
+    case isWorking
+    case isStopped
+}
+
+
 class ClockViewController: UIViewController {
     
     var senderTag = 0
+    var boilTime = (0, 0)
     @IBOutlet weak var clock: Clocket!
+    @IBOutlet weak var startButton: CustomButton!
+    var clockState : clockState = .isStopped
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        clock.displayRealTime = true
-        clock.startClock()
+        setupClock()
         
-
+        clock.clockDelegate = self
+        
         // Do any additional setup after loading the view.
     }
     
-
-   
-
+    
+    private func setupClock() {
+        switch senderTag {
+        case 0:
+            boilTime = (4,30)
+        case 1:
+            boilTime = (6,30)
+        case 2:
+            boilTime = (8,30)
+        default:
+            print("Unexpected value for senderTag.")
+        }
+        
+        
+        clock.countDownTimer = true
+        clock.displayRealTime = false
+        clock.setLocalTime(hour: 0, minute: boilTime.0, second: boilTime.1)
+        clock.reverseTime = true
+        clock.isUserInteractionEnabled = false
+        
+        startButton.setTitle("Start Timer", for: .normal)
+        
+    }
+    
+    @IBAction func startButtonTapped(_ sender: CustomButton) {
+        if clockState == .isStopped {
+            clock.startClock()
+            startButton.setTitle("Pause Timer", for: .normal)
+            clockState = .isWorking
+        } else {
+            clock.stopClock()
+            startButton.setTitle("Start Timer", for: .normal)
+            clockState = .isStopped
+        }
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: CustomButton) {
+        //clock.stopClock()
+        setupClock()
+        
+        startButton.isHidden = false
+        startButton.isEnabled = true
+    }
+    
 }
+
+extension ClockViewController : ClocketDelegate {
+    func timeIsSetManually() {
+        
+    }
+    
+    func clockStopped() {
+        clockState = .isStopped
+    }
+    
+    func countDownExpired() {
+        SweetAlert().showAlert("Enjoy your meal!", subTitle: "Your eggs are ready.", style: .success, buttonTitle: "OK")
+        startButton.isHidden = true
+        startButton.isEnabled = false
+    }
+    
+    
+}
+
+
